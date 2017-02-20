4328dd5fe5340f77fe1fb17729bf777396434ad0
Alan Hamlett <alan.hamlett@gmail.com>
2017-02-19 20:18:13 -0800

    two spaces indentation, even though M$ TypeScript style guide says 4

--- src/editors/editor.ts
+++ src/editors/editor.ts
@@ -1,10 +1,10 @@
 declare interface Editor {
-    name: string;
-    icon: string;
-    isEditorInstalled(): Promise<boolean>;
-    isPluginInstalled(): Promise<boolean>;
-    installPlugin(): Promise<void>;
-    uninstallPlugin(): Promise<void>;
+  name: string;
+  icon: string;
+  isEditorInstalled(): Promise<boolean>;
+  isPluginInstalled(): Promise<boolean>;
+  installPlugin(): Promise<void>;
+  uninstallPlugin(): Promise<void>;
 }
 
 export default Editor;

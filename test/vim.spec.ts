4130bbb77858057f77aa418d94ba22d21cc43b8d
Carlos Henrique Guardao Gandarez <gandarez@gmail.com>
2019-05-14 15:44:48 -0300

    replaced dependency from ts-mock-imports to sinon

--- test/vim.spec.ts
+++ test/vim.spec.ts
@@ -1,35 +1,32 @@
-import { ImportMock, MockManager } from 'ts-mock-imports';
 import Vim from "../src/editors/vim";
-import *  as CommandExists from '../src/lib/command-exists';
 
-const chai = require('chai');  
-const chaiAsPromised = require('chai-as-promised');  
-  
-const { expect } = chai;  
+var sinon = require('sinon');
+
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+
+const { expect } = chai;
 chai.use(chaiAsPromised);
 
 describe('Vim', () => {
-    let commandExistsMock: MockManager<CommandExists.default>;
     let vim: Vim;
 
     beforeEach(() => {
-        commandExistsMock = ImportMock.mockClass(CommandExists, 'default');
         vim = new Vim();
     });
     afterEach(() => {
-        commandExistsMock.restore();
     });
-    it('should return the correct editor name', async () => {
-        const result = await vim.name;
+    it('should return the correct editor name', () => {
+        const result = vim.name;
         expect(result).to.equal('vim');
     });
     it('should return TRUE if editor is installed', async () => {
-        commandExistsMock.mock('exists', Promise.resolve(true));
+        sinon.stub(vim, 'isEditorInstalled').returns(Promise.resolve(true));
         const result = await vim.isEditorInstalled();
         expect(result).to.be.true;
     });
     it('should return FALSE if editor is not installed', async () => {
-        commandExistsMock.mock('exists', Promise.resolve(false));
+        sinon.stub(vim, 'isEditorInstalled').returns(Promise.resolve(false));
         const result = await vim.isEditorInstalled();
         expect(result).to.be.false;
     });

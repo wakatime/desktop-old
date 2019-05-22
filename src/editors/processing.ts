c00f4936a8b2aae32d2745e666fc9f743a763a4b
Carlos Henrique Guardao Gandarez <gandarez@gmail.com>
2019-05-22 19:11:42 -0300

    fix switch for processing

--- src/editors/processing.ts
+++ src/editors/processing.ts
@@ -102,6 +102,7 @@ export default class Processing implements Editor {
                         });
                     }
                 }
+                break;
             default:
                 break;
         }

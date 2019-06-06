b586421f0a6ab004d47f953e40486647c4c4b17b
Carlos Henrique Guardao Gandarez <gandarez@gmail.com>
2019-06-06 19:12:28 -0300

    initial implementation for Google Chrome

---
+++ src/editors/chrome.ts
@@ -0,0 +1,48 @@
+import Editor from "./editor";
+import * as os from 'os';
+import * as fs from 'async-file'
+
+export default class Chrome implements Editor
+{
+    
+    public get name(): string {
+        return 'chrome'
+    }
+
+    public get displayName(): string {
+        return 'Google Chrome'
+    }
+
+    public get icon(): string {
+        return ''
+    }
+
+    public async isEditorInstalled(): Promise<boolean> {
+        return await this.isDirectory(this.appDirectory());
+    }
+    public async isPluginInstalled(): Promise<boolean> {
+        throw new Error("Method not implemented.");
+    }
+    public async installPlugin(): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+    public async uninstallPlugin(): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+
+    public async isDirectory(directory: string): Promise<boolean> {
+        let stats = await fs.stat(directory);
+        return stats.isDirectory();
+    }
+
+    private appDirectory(): string {
+        switch (os.platform()) {
+            case 'win32':
+                return '';
+            case 'darwin':
+                return '/Applications/Google Chrome.app/Contents';
+            default:
+                return null;
+        }
+    }
+}

13e9cea4018d6071cd9060e5df0715caf86561a4
Carlos Henrique Guardao Gandarez <gandarez@gmail.com>
2019-06-27 11:41:01 -0300

    initial implementation for eclipse editor

---
+++ src/editors/eclipse.ts
@@ -0,0 +1,77 @@
+import Editor from "./editor";
+import * as os from 'os';
+import * as fs from 'async-file'
+import * as fs_sync from 'fs';
+import * as path from 'path'
+
+export default class Eclipse implements Editor {
+
+    public get name(): string {
+        return 'eclipse'
+    }
+
+    public get displayName(): string {
+        return 'Eclipse'
+    }
+
+    public get icon(): string {
+        return ''
+    }
+
+    public async isEditorInstalled(): Promise<boolean> {
+        return await this.isDirectory(this.appDirectory());
+    }
+
+    public async isPluginInstalled(): Promise<boolean> {
+        throw new Error("Method not implemented.");
+    }
+
+    public async installPlugin(): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+
+    public async uninstallPlugin(): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+
+    public async isDirectory(directory: string): Promise<boolean> {
+        let stats = await fs.stat(directory);
+        return stats.isDirectory();
+    }
+
+    private appDirectory(): string {
+        switch (os.platform()) {
+            case 'win32':
+                return '';
+            case 'darwin':
+                //find a way of recursively find Eclipse.app folder
+                return path.join(os.homedir(), 'eclipse/java-2019-06/Eclipse.app/Contents');
+            default:
+                return null;
+        }
+    }
+
+    // private async findDirectory(dir: string, folder: string): Promise<boolean> {
+    //     var results = [];
+    //     fs_sync.readdir(dir, function (err, list) {
+    //         if (err) return false;
+    //         var pending = list.length;
+    //         if (!pending) return false;
+    //         list.forEach(function (file) {
+    //             file = path.resolve(dir, file);
+    //             fs_sync.stat(file, function (_, stat) {
+    //                 if (stat && stat.isDirectory()) {
+    //                     return true;
+    //                     this.findDirectory(file, function (err, res) {
+    //                         results = results.concat(res);
+    //                         if (!--pending) done(null, results);
+    //                     });
+    //                 } else {
+    //                     results.push(file);
+    //                     if (!--pending) done(null, results);
+    //                 }
+    //             });
+    //         });
+    //     });
+    // };
+}

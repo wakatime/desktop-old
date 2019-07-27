a0aa30acc5a8dc11579f2c04a1694609fff3073c
Alan Hamlett <alan.hamlett@gmail.com>
2019-07-27 11:35:07 -0700

    fix minification

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,36 @@
 /**
  * Created by v0n000p on 2/19/17.
  */
+
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
 const path = require('path');
 const webpack = require('webpack');
-const isProduction = (process.env.NODE_ENV === 'production');
+const isProduction = process.env.NODE_ENV === 'production';
 
 console.log('isProduction', isProduction);
 
 const plugins = [];
-const loaders = [
-    { test: /\.tsx?$/, loader: 'ts-loader' }
-];
-if(isProduction){
-    plugins.push(
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            }
-        })
-    )
-}else {
+const optimization = {};
+const loaders = [{ test: /\.tsx?$/, loader: 'ts-loader' }];
 
+if (isProduction) {
+  optimization.minimizer = [new UglifyJsPlugin()];
 }
+
 module.exports = {
-    entry: path.resolve(__dirname, './src/containers/index.ts'),
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: '[name].js'
-    },
-    resolve: {
-        // Add `.ts` and `.tsx` as a resolvable extension.
-        extensions: ['.ts', '.tsx', '.js'] // note if using webpack 1 you'd also need a '' in the array as well
-    },
-    module:{
-        loaders:loaders
-    },
-    plugins:plugins
+  entry: path.resolve(__dirname, './src/containers/index.ts'),
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].js',
+  },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: ['.ts', '.tsx', '.js'], // note if using webpack 1 you'd also need a '' in the array as well
+  },
+  module: {
+    loaders: loaders,
+  },
+  optimization: optimization,
+  plugins: plugins,
 };

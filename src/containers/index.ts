8370080588ba98aa870cfa2c5cc17646dc632da2
Vu Nguyen <shardphoneix@gmail.com>
2019-07-27 22:25:13 -0700

    Cleanup

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,7 +1,2 @@
-/**
- * Created by v0n000p on 2/19/17.
- */
-import test from "./test";
-
 // eslint-disable-next-line no-console
-console.log("Hello world script", test);
+console.log("Hello world script");

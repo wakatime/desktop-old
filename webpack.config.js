75af2d95226a4e6f04f70b6186a8b1bca339116c
Vu Nguyen <shardphoneix@gmail.com>
2019-07-27 23:05:35 -0700

    add nodemon to have app autorestart on change

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,7 @@
-/**
- * Created by v0n000p on 2/19/17.
- */
-
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-
 const path = require("path");
-// eslint-disable-next-line no-unused-vars
-const webpack = require("webpack");
-
-const isProduction = process.env.NODE_ENV === "production";
-
-// eslint-disable-next-line no-console
-console.log("isProduction", isProduction);
-
-const plugins = [];
-const optimization = {};
-
-if (isProduction) {
-  optimization.minimizer = [new UglifyJsPlugin()];
-}
 
 module.exports = {
+  devtool: "inline-cheap-source-map",
   entry: path.resolve(__dirname, "./src/containers/index.ts"),
   output: {
     path: path.resolve(__dirname, "./dist"),
@@ -42,7 +23,5 @@ module.exports = {
         ]
       }
     ]
-  },
-  optimization,
-  plugins
+  }
 };

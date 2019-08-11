e33930d22c620b73955708ceedc33338b4b4ab3f
Vu Nguyen <shardphoneix@gmail.com>
2019-08-11 09:56:35 -0700

    Fixes for lint warnings

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,9 @@
 import * as electron from "electron";
-import { join } from "path";
 import installExtension, {
   REACT_DEVELOPER_TOOLS
 } from "electron-devtools-installer";
-import EditorManager from "./editorManager";
 
 const isDev = process.env.DEV === "true";
-const { Menu, Tray } = electron;
 // Module to control application life.
 const { app } = electron;
 // Module to create native browser window.
@@ -15,13 +12,13 @@ const { BrowserWindow } = electron;
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
-let appIcon;
 
 function createWindow() {
   // Create the browser window.
   mainWindow = new BrowserWindow({ width: 800, height: 600 });
 
   // and load the index.html of the app.
+  // eslint-disable-next-line no-console
   console.log(`Starting in dev mode? ${isDev}`);
   if (isDev) {
     // Attempt to load window until successful
@@ -30,9 +27,12 @@ function createWindow() {
       try {
         mainWindow.loadURL("http://localhost:8080");
         installExtension(REACT_DEVELOPER_TOOLS)
+          // eslint-disable-next-line no-console
           .then(name => console.log(`Added Extension:  ${name}`))
+          // eslint-disable-next-line no-console
           .catch(err => console.log("An error occurred: ", err));
       } catch (e) {
+        // eslint-disable-next-line no-console
         console.error("Unable to load page, waiting 500ms to retry...");
         setTimeout(loadWin, 500);
       }
@@ -40,7 +40,6 @@ function createWindow() {
     setTimeout(loadWin, 500);
   } else {
     const indexHtmlPath = `file://${__dirname}/index.html`;
-    console.log("indexHTMLPATH", indexHtmlPath);
     mainWindow.loadURL(indexHtmlPath);
   }
 
@@ -50,7 +49,7 @@ function createWindow() {
   }
 
   // Emitted when the window is closed.
-  mainWindow.on("closed", function() {
+  mainWindow.on("closed", () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
@@ -75,7 +74,7 @@ app.on("ready", () => {
 });
 
 // Quit when all windows are closed.
-app.on("window-all-closed", function() {
+app.on("window-all-closed", () => {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== "darwin") {
@@ -83,7 +82,7 @@ app.on("window-all-closed", function() {
   }
 });
 
-app.on("activate", function() {
+app.on("activate", () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {

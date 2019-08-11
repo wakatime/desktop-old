c14154c801ba1494433cf0e59fa022a01e9fc99b
Vu Nguyen <shardphoneix@gmail.com>
2019-08-11 13:14:59 -0700

    modify webpack, and app to allow for import of electron

--- src/containers/index.tsx
+++ src/containers/index.tsx
@@ -1,7 +1,9 @@
-import * as React from "react";
+import React from "react";
 import { render } from "react-dom";
+import electron from 'electron';
 import AllIconImgs from "../components/AllIconImgs";
 
+console.log('electron', electron)
 const div = document.getElementById("container");
 render(
   <div>

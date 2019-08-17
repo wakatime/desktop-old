892ed5f3af7e111a958411f47945a80d3025512a
Vu Nguyen <shardphoneix@gmail.com>
2019-08-17 11:52:59 -0700

    Create a constants folder for each type of editor

---
+++ src/constants/editors.ts
@@ -0,0 +1,9 @@
+export const ANDROIDSTUDIO = "ANDROIDSTUDIO";
+export const ATOM = "ATOM";
+export const CHROME = "CHROME";
+export const ECLIPSE = "ECLIPSE";
+export const SUBLIMETEXT2 = "SUBLIMETEXT2";
+export const SUBLIMETEXT3 = "SUBLIMETEXT3";
+export const VIM = "VIM";
+export const VSCODE = "VSCODE";
+export const XCODE = "XCODE";

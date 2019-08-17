a312fb4075ac3d2029172f6894aa4dd1bac2f074
Vu Nguyen <shardphoneix@gmail.com>
2019-08-17 12:00:47 -0700

    Create an image map to make it more configurable which Editor to which image

---
+++ src/config/editorsImgMap.ts
@@ -0,0 +1,36 @@
+import {
+  ANDROIDSTUDIO,
+  ATOM,
+  CHROME,
+  ECLIPSE,
+  SUBLIMETEXT2,
+  SUBLIMETEXT3,
+  VIM,
+  VSCODE
+} from "../constants/editors";
+import {
+  androidStudio128Path,
+  atom128Path,
+  chrome128Path,
+  eclipse128Path,
+  sublimeText128Path,
+  vim128Path,
+  vsCode128Path
+} from "../constants/imgPaths";
+
+interface EditorImageMap {
+  [s: string]: string;
+}
+
+const imgMap: EditorImageMap = {
+  [ANDROIDSTUDIO]: androidStudio128Path,
+  [ATOM]: atom128Path,
+  [CHROME]: chrome128Path,
+  [ECLIPSE]: eclipse128Path,
+  [SUBLIMETEXT2]: sublimeText128Path,
+  [SUBLIMETEXT3]: sublimeText128Path, // Should this be a different image?
+  [VIM]: vim128Path,
+  [VSCODE]: vsCode128Path
+};
+
+export default imgMap;

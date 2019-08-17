91c5248217c0d9180ce1830b9bf5fa12544fa308
Vu Nguyen <shardphoneix@gmail.com>
2019-08-17 12:55:46 -0700

    Remove console statement

--- src/containers/index.tsx
+++ src/containers/index.tsx
@@ -4,9 +4,6 @@ import { render } from "react-dom";
 import ActiveEditors from "../components/ActiveEditors";
 import store from "../stores/rendererStore";
 
-console.log("store", store);
-console.log("store state:", store.getState());
-
 const div = document.getElementById("container");
 render(
   <Provider store={store}>

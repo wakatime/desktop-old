17a65664673cad5261bb5725949a18f10eae5d40
Vu Nguyen <shardphoneix@gmail.com>
2019-08-25 03:10:05 -0700

    refactor webpack config to be a function to get access to mode in order to determine if is prod/dev mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,72 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const isDev = process.env.DEV === "true";
-const output = {
-  path: path.resolve(__dirname, "./dist"),
-  filename: "[name].js"
-};
-const devtool = isDev ? "source-map" : undefined;
-const resolve = {
-  // Add `.ts` and `.tsx` as a resolvable extension.
-  extensions: [".ts", ".tsx", ".js", ".json"] // note if using webpack 1 you'd also need a '' in the array as well
-};
-const tsloader = {
-  test: /\.tsx?$/,
-  exclude: /node_modules/,
-  use: [
-    {
-      loader: "ts-loader"
-    }
-  ]
-};
-const fileLoader = {
-  test: /\.(png|jpe?g|gif)$/,
-  use: [
-    {
-      loader: "file-loader",
-      options: {
-        name() {
-          if (isDev) {
-            return "[path][name].[ext]";
+module.exports = (_env, _cfg) => {
+  const isDev = _cfg.mode === "production";
+  const output = {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "[name].js"
+  };
+  const devtool = isDev ? "source-map" : undefined;
+  const resolve = {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: [".ts", ".tsx", ".js", ".json"] // note if using webpack 1 you'd also need a '' in the array as well
+  };
+  const tsloader = {
+    test: /\.tsx?$/,
+    exclude: /node_modules/,
+    use: [
+      {
+        loader: "ts-loader"
+      }
+    ]
+  };
+  const fileLoader = {
+    test: /\.(png|jpe?g|gif)$/,
+    use: [
+      {
+        loader: "file-loader",
+        options: {
+          name() {
+            if (isDev) {
+              return "[path][name].[ext]";
+            }
+            return "assets/[hash].[ext]";
           }
-          return "assets/[hash].[ext]";
         }
       }
+    ]
+  };
+  const commonWebpackCfg = {
+    devtool,
+    output,
+    resolve,
+    module: {
+      rules: [tsloader, fileLoader]
     }
-  ]
+  };
+  const mainProcessWebpackCfg = {
+    ...commonWebpackCfg,
+    target: "electron-main",
+    entry: {
+      app: path.resolve(__dirname, "./src/app.ts")
+    },
+    node: {
+      __dirname: isDev
+    },
+    plugins: []
+  };
+  const rendererWebpackCfg = {
+    ...commonWebpackCfg,
+    target: "electron-renderer",
+    entry: {
+      mainUI: path.resolve(__dirname, "./src/containers/index.tsx")
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "src", "html", "index.html")
+      })
+    ]
+  };
+  return [mainProcessWebpackCfg, rendererWebpackCfg];
 };
-const commonWebpackCfg = {
-  devtool,
-  output,
-  resolve,
-  module: {
-    rules: [tsloader, fileLoader]
-  }
-};
-const mainProcessWebpackCfg = {
-  ...commonWebpackCfg,
-  target: "electron-main",
-  entry: {
-    app: path.resolve(__dirname, "./src/app.ts")
-  },
-  node: {
-    __dirname: isDev
-  },
-  plugins: []
-};
-const rendererWebpackCfg = {
-  ...commonWebpackCfg,
-  target: "electron-renderer",
-  entry: {
-    mainUI: path.resolve(__dirname, "./src/containers/index.tsx")
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src", "html", "index.html")
-    })
-  ]
-};
-module.exports = [mainProcessWebpackCfg, rendererWebpackCfg];

33b8a8e349af4dc3c79f95d7ba71ea42e3ab7a88
Vu Nguyen <shardphoneix@gmail.com>
2019-08-25 13:56:56 -0700

    Add typing and logger middleware

---
+++ src/middlewares/logger.ts
@@ -0,0 +1,20 @@
+import { Store, Dispatch, Action } from "redux";
+
+const crashReporter = (store: Store) => (next: Dispatch) => (
+  action: Action
+) => {
+  const { getState } = store;
+  const beforeActionState = getState();
+  const result = next(action);
+  const afterActionState = getState();
+  console.info(
+    "action",
+    action,
+    "beforeState",
+    beforeActionState,
+    "afterState",
+    afterActionState
+  );
+  return result;
+};
+export default crashReporter;

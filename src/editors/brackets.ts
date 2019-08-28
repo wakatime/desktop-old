a0feafa7295d3470c57a5f024499519e749225eb
Carlos Henrique Guardao Gandarez <gandarez@gmail.com>
2019-08-28 17:00:57 -0300

    initial implementation for brackets editor

---
+++ src/editors/brackets.ts
@@ -0,0 +1,55 @@
+import os from "os";
+import Editor from "./editor";
+
+export default class Brackets extends Editor {
+  public get name(): string {
+    return "Brackets";
+  }
+
+  public get icon(): string {
+    return "";
+  }
+
+  public async isEditorInstalled(): Promise<boolean> {
+    return await this.isDirectory(this.appDirectory());
+  }
+
+  public async isPluginInstalled(): Promise<boolean> {
+    return await this.isDirectory(this.appDirectory());
+  }
+
+  public async installPlugin(): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  public async uninstallPlugin(): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  private appDirectory(): string {
+    switch (os.platform()) {
+      case "win32":
+        return "C:\\Program Files (x86)\\Brackets";
+      case "darwin":
+        return "/Applications/Brackets.app/Contents";
+      case "linux":
+        return null;
+      default:
+        return null;
+    }
+  }
+
+  private pluginsDirectory(): string {
+    switch (os.platform()) {
+      case "win32": {
+        return "%USERPROFILE\\AppData\\Roaming\\Brackets\\extensions\\user\\brackets-wakatime";
+      }
+      case "darwin":
+        return "/Users/gandarez/Library/Application Support/Brackets/extensions/user/brackets-wakatime";
+      case "linux":
+        return "~/.config/brackets/extensions/user/brackets-wakatime";
+      default:
+        return null;
+    }
+  }
+}

3e39da1dbb607db6749868a649e3d940d3b8958b
jvelezpo <sebasvelez@gmail.com>
2019-11-03 08:44:06 -0500

    Sorts the editors by the ones with the plugin installed first

--- src/utils/editors.tsx
+++ src/utils/editors.tsx
@@ -1,9 +1,19 @@
 /**
+ * Sorts the editors by the ones with the plugin installed first
+ * @param editors 
+ */
+const orderByInstalledPlugin = (editors) => {
+  return editors.sort((x, y) => {
+    return (x.enabled === y.enabled)? 0 : x.enabled? -1 : 1;
+  })
+}
+
+/**
  * Returns the current state of local editors, if they are installed and enable
  * @param editors
  */
 export const getEditorsState = async editors => {
-  return await Promise.all(
+  const editorsList = await Promise.all(
     editors.map(async editor => {
       // This try catch will go away once the `isPluginInstalled` is implemented on each editor
       // right now we need to have it so it wont blow up at this point
@@ -20,4 +30,6 @@ export const getEditorsState = async editors => {
       };
     }),
   );
+
+  return orderByInstalledPlugin(editorsList);
 };

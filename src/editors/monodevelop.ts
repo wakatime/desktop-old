0fe2c01ea8509d8124e2a0e86af8550b996fc4c7
jvelezpo <sebasvelez@gmail.com>
2019-12-02 11:17:22 -0500

    add MonoDevelop basics, returns empty for mac

---
+++ src/editors/monodevelop.ts
@@ -0,0 +1,60 @@
+import os from 'os';
+import path from 'path';
+
+import Editor from './editor';
+
+export default class MonoDevelop extends Editor {
+  public static getName(): string {
+    return 'MonoDevelop';
+  }
+
+  public get name(): string {
+    return 'MonoDevelop';
+  }
+
+  public get icon(): string {
+    return '';
+  }
+
+  public async isEditorInstalled(): Promise<boolean> {
+    return await this.isDirectory(this.appDirectory());
+  }
+
+  public async isPluginInstalled(): Promise<boolean> {
+    return await this.isDirectory(this.pluginsDirectory());
+  }
+
+  public async installPlugin(): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+
+  public async uninstallPlugin(): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+
+  public pluginsDirectory(): string {
+    switch (os.platform()) {
+      case 'win32': {
+        return '';
+      }
+      case 'darwin': {
+        return ''; // return empty because MonoDevelop is not available in mac
+      }
+      case 'linux':
+        return '';
+      default:
+        return null;
+    }
+  }
+
+  public appDirectory(): string {
+    switch (os.platform()) {
+      case 'win32':
+        return '';
+      case 'darwin':
+        return ''; // return empty because MonoDevelop is not available in mac
+      default:
+        return null;
+    }
+  }
+}

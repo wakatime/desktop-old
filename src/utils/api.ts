dc5bc60f53475e11ac729d8ff9a74bf3c5f8e118
jvelezpo <sebasvelez@gmail.com>
2020-05-25 17:41:37 -0500

    set wakatime api url in const and make summaries call more flexible for future uses

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,14 +3,16 @@ import axios from 'axios';
 import Options from './options';
 
 const options = new Options();
+const baseURL = 'https://wakatime.com';
+
 export default class API {
-  public async summaries() {
+  public async summaries(start: string, end: string) {
     try {
       const apikey = await options.getApiKeyAsync();
       const token = Buffer.from(apikey).toString('base64');
-      axios.defaults.baseURL = 'https://wakatime.com';
+      axios.defaults.baseURL = baseURL;
       axios.defaults.headers.common.Authorization = `Basic ${token}`;
-      const response = await axios.get('/api/v1/users/current/summaries?start=today&end=today');
+      const response = await axios.get(`/api/v1/users/current/summaries?start=${start}&end=${end}`);
       if (response.status !== 200) {
         return null;
       }
@@ -20,8 +22,8 @@ export default class API {
     }
   }
 
-  public async todayMins() {
-    const summaries = await this.summaries();
+  public async todayMins(): Promise<string> {
+    const summaries = await this.summaries('today', 'today');
     if (summaries == null) {
       return '0 min';
     }
